e9508ce5272ed49dcb4e40037f45b74ddd9ed755
George Diaz <georgediaz88@yahoo.com>
2011-10-23 14:48:41 -0500

    Clean spec with path ; Take out URL

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -8,7 +8,7 @@ describe "Tasks" do
   end
 
   def login_usr
-    visit '/users/sign_in'
+    visit new_user_session_path
     fill_in 'Email', :with => @user.email
     fill_in 'Password', :with => @user.password
     click_button 'Sign in'
